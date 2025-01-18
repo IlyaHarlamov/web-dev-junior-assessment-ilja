c13f2cc67dc77dc9e009fab9bd0f1c46ce256385
Ilja Harlamovs <ilya.harlamovv@gmail.com>
2025-01-18 20:17:14 +0200

    Formatting; Implemented the avatar upload and preview; Finished the ticket component

--- js/formHandler.js
+++ js/formHandler.js
@@ -1,17 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("ticket-form");
 
-  form.addEventListener("submit", (event) => {
+  form.addEventListener("submit", async (event) => {
     event.preventDefault();
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
-    setMainWelcomeMessageInnerHtml(data["full-name"]);
-    setSecondaryWelcomeMessageInnerHTML(data["email"]);
+
+    const fullname = data['full-name'];
+    const email = data['email'];
+    const githubUsername = data['github-username'];
+    const avatarURL = URL.createObjectURL(data['avatar']);
+    setMainWelcomeMessageInnerHtml(fullname);
+    setSecondaryWelcomeMessageInnerHTML(email);
     hideTicketFormContainer();
-    displayTicket();
+    await displayTicket(fullname, githubUsername, avatarURL);
   });
 });
 
+/* Welcome Section */
 function setMainWelcomeMessageInnerHtml(fullName) {
   getMainWelcomeMessage().innerHTML = `Congrats, <span id="welcome-message-fullname">${fullName}</span>!
     <br>Your ticket is ready.`;
@@ -37,6 +43,7 @@ function getSecondaryWelcomeMessage() {
   return getWelcomeSection().querySelector("p");
 }
 
+/* Ticket */
 function getTicketFormContainer() {
   return document.getElementById("ticket-form-container");
 }
@@ -45,10 +52,61 @@ function hideTicketFormContainer() {
     getTicketFormContainer().style.display = "none";
 }
 
-function displayTicket() {
-    fetch("html/ticket-component.html")
-    .then((response) => response.text())
-    .then((html) => {
-        document.getElementById('ticket-placeholder').innerHTML = html;
-    });
+async function displayTicket(fullname, githubUsername, avatarURL) {
+    const response = await fetch("html/ticket-component.html");
+    const html = await response.text();
+    document.getElementById('ticket-placeholder').innerHTML = html;
+    const elFullname = document.getElementById('ticket-fullname');
+    const elGithubUsername = document.getElementById('ticket-github-username');
+    const elAvatar = document.getElementById('ticket-avatar');
+    elFullname.textContent = fullname;
+    elGithubUsername.textContent = githubUsername;
+    elAvatar.src = avatarURL;
+}
+
+/* File Upload */
+function getPreviewContainer() {
+  return document.getElementById('preview-container');
+}
+
+function getAvatarInput() {
+  return document.getElementById('avatar');
+}
+
+function getPreviewImage() {
+  return document.getElementById('preview-image');
+}
+
+function getUploadLabel() {
+  return document.getElementById('ticket-form-file-upload');
+}
+
+function handleFileUpload(event) {
+  const file = event.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    // Once the file is read, display it
+    reader.onload = function (e) {
+      getPreviewImage().src = e.target.result;
+      getUploadLabel().style.display = 'none';
+      getPreviewContainer().style.display = 'flex';
+    };
+
+    reader.readAsDataURL(file);
+  }
+}
+
+function removeImage() {
+  // Reset file input and hide preview
+  getAvatarInput().value = '';
+  getPreviewImage().src = '';
+  getPreviewContainer().style.display = 'none';
+  getUploadLabel().style.display = 'flex'; // Show the upload label again
+}
+
+function reuploadImage() {
+  // Simulate a click on the file input
+  getAvatarInput().click();
 }
